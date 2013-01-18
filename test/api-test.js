0d7d87c08a4cd06dc43a462a7b1fe7452d1b40f3
Fedor Indutny <fedor.indutny@gmail.com>
2013-01-18 14:47:54 +0400

    api: fixes and test

--- test/api-test.js
+++ test/api-test.js
@@ -8,17 +8,24 @@ describe('Spoon', function() {
   describe('spoon() API function', function() {
     it('should work properly', function() {
       var code = spoon(function a(__$callback) {
+        function async(callback) {
+          callback(null, 1);
+        }
         "enable spoon";
-        return 1;
+        return 1 + async();
       }.toString(), ['async'], {
         declaration: 'enable spoon'
       });
 
+      var called = false;
       vm.runInNewContext(code + ';a(callback);', {
         callback: function(err, value) {
-          assert.equal(value, 1);
+          called = true;
+          assert.equal(value, 2);
         }
       });
+
+      assert(called);
     });
   });
 });
